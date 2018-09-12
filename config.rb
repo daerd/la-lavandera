f252367e0196c112a1cd7c02cfa1a08ed471b8cb
d-herrero <daniel.herrero.101@gmail.com>
2018-09-12 15:26:23 +0200

    Navigation logic and CSS finished.

--- config.rb
+++ config.rb
@@ -27,6 +27,82 @@ end
 
 activate :i18n, mount_at_root: :es
 
+################
+# PAGES CONFIG #
+################
+
+# Navigation
+config[:navigation] = [
+  {
+    key:  'home',
+    text: 'navigation.home',
+    link: '/'
+  },
+  {
+    key:  'services',
+    text: 'navigation.services.text',
+    link: '#',
+    items: [
+      {
+        key: 'laundry',
+        text: 'navigation.services.items.laundry',
+        link: '/laundry.html'
+      },
+      {
+        key:  'dry_cleaning',
+        text: 'navigation.services.items.dry_cleaning',
+        link: '/dry-cleaning.html'
+      },
+      {
+        key: 'leather_and_suede',
+        text: 'navigation.services.items.leather_and_suede',
+        link: '/leather-and-suede.html'
+      },
+      {
+        key: 'wedding_and_event_clothing',
+        text: 'navigation.services.items.wedding_and_event_clothing',
+        link: '/wedding-and-event-clothing.html'
+      },
+      {
+        key:  'uniforms',
+        text: 'navigation.services.items.uniforms',
+        link: '/uniforms.html'
+      },
+      {
+        key:  'linen',
+        text: 'navigation.services.items.linen',
+        link: '/linen.html'
+      }
+    ]
+  },
+  {
+    key:  'prices',
+    text: 'navigation.prices',
+    link: '/prices.html'
+  },
+  {
+    key:  'faq',
+    text: 'navigation.faq',
+    link: '/faq.html'
+  },
+  {
+    key:  'deals',
+    text: 'navigation.deals',
+    link: '/deals.html'
+  },
+  {
+    key:  'contact',
+    text: 'navigation.contact',
+    link: '/contact.html'
+  }
+]
+
+# Service pages
+config[:navigation].select{ |x| x[:key] == 'services' }.first[:items].map{ |x| x[:key] }.each do |service|
+  proxy "/#{service.gsub('_', '-')}.html", '/service.html', locals: { service: service }
+end
+
+# Google Maps
 config[:gmaps_api_key] = 'gmaps_api_key'
 config[:gmaps_lat]     = '40.4298498'
 config[:gmaps_lng]     = '-3.6422492'
